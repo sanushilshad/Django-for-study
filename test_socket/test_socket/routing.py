46c37d88a6fdc9bf813bbefb9844085faa24178a
sanu shilshad <sanushilshad@gmail.com>
2021-12-23 23:54:27 +0530

    added path for the websocket

--- test_socket/test_socket/routing.py
+++ test_socket/test_socket/routing.py
@@ -1,6 +1,10 @@
 from channels.routing import route
-from location.consumers import ws_message
+from location.consumers import ws_add, ws_message, ws_disconnect
 
 channel_routing = [
-    route("websocket.receive", ws_message),
+    route("websocket.connect", ws_add, path=r"^/chat/$"),
+    route("websocket.receive", ws_message, path=r"^/chat/$"),
+    route("websocket.disconnect", ws_disconnect, path=r"^/chat/$"),
 ]
+
+
