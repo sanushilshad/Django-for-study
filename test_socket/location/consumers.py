9757fedddcf5cecbf7a6543c3e48c130f056c8f4
Kevin Norbert <kevin.norbert@acelrtech.com>
2021-12-24 13:16:22 +0530

    Group Intro (kevin)

--- test_socket/location/consumers.py
+++ test_socket/location/consumers.py
@@ -1,11 +1,38 @@
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
+from channels import Group
 import time
+import json
+import urlparse
 
 def ws_message(message):
     # ASGI WebSocket packet-received and send-packet message types
     # both have a "text" key for their textual data.
-    message.reply_channel.send({
-        "text": message.content['text'] + ' Kevin',
+    json_data = json.loads(message.content['text'])
+    user = json_data['user']
+    start_time = json_data['start_time']
+    end_time = json_data['end_time']
+    date = json_data['date']
+    group_name = clean_query_string('{}-{}-{}-{}'.format(user, date, start_time, end_time))
+    Group(group_name).send({
+        "text": user,
     })
+
+def ws_connect(message):
+    # Accept the connection
+    message.reply_channel.send({"accept": True})
+    json_data = urlparse.parse_qs(message.content['query_string'])
+    user = json_data['user']
+    date = json_data['date']
+    start_time = json_data['start_time']
+    end_time = json_data['end_time']
+    group_name = clean_query_string('{}-{}-{}-{}'.format(user, date, start_time, end_time))
+    Group(group_name).add(message.reply_channel)
+
+def clean_query_string(query_string):
+    query_string = query_string.replace('[', '')
+    query_string = query_string.replace(']', '')
+    query_string = query_string.replace("'", '')
+    query_string = query_string.replace('"', '')
+    return query_string
         
--- test_socket/test_socket/routing.py
+++ test_socket/test_socket/routing.py
@@ -1,6 +1,7 @@
 from channels.routing import route
-from location.consumers import ws_message
+from location.consumers import ws_message, ws_connect
 
 channel_routing = [
     route("websocket.receive", ws_message),
+    route("websocket.connect", ws_connect),
 ]
